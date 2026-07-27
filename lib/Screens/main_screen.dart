7962a0a20cfba38f2c941bdc84d564a66f73fb8f
sanfangx <x340711@icloud.com>
2026-07-27 16:10:33 +0800

    fix: clear image cache on background to prevent JetSam termination, downgrade Flutter to 3.24.5

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -640,6 +640,8 @@ class MainScreenState extends State<MainScreen>
         cancleTimer();
         break;
       case AppLifecycleState.paused:
+        PaintingBinding.instance.imageCache.clear();
+        PaintingBinding.instance.imageCache.clearLiveImages();
         setTimer();
         break;
       case AppLifecycleState.detached:
