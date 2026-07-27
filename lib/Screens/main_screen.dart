ecb7712f43fadf3f8fe12616ece2c138fe7deed4
sanfangx <x340711@icloud.com>
2026-07-27 13:52:14 +0800

    fix: forcefully clear image cache on background to evade TrollStore Jetsam limits

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -641,11 +641,15 @@ class MainScreenState extends State<MainScreen>
         cancleTimer();
         break;
       case AppLifecycleState.paused:
+        PaintingBinding.instance.imageCache.clear();
+        PaintingBinding.instance.imageCache.clearLiveImages();
         setTimer();
         break;
       case AppLifecycleState.detached:
         break;
       case AppLifecycleState.hidden:
+        PaintingBinding.instance.imageCache.clear();
+        PaintingBinding.instance.imageCache.clearLiveImages();
         break;
     }
   }
