4da03feed6271c951967acf19c4a936d328feebf
sanfangx <x340711@icloud.com>
2026-07-27 17:03:34 +0800

    fix: add UIBackgroundTaskIdentifier to prevent 0xDEAD10CC termination by RUNNINGBOARD on SQLite/Hive file locks during background suspension

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,30 @@ import UIKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  var backgroundTask: UIBackgroundTaskIdentifier = .invalid
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func applicationDidEnterBackground(_ application: UIApplication) {
+    // Request extra time from iOS to allow SQLite and Hive to finish pending writes and release file locks
+    backgroundTask = application.beginBackgroundTask(withName: "SQLiteLockPreventer") {
+      application.endBackgroundTask(self.backgroundTask)
+      self.backgroundTask = .invalid
+    }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+      if self.backgroundTask != .invalid {
+        application.endBackgroundTask(self.backgroundTask)
+        self.backgroundTask = .invalid
+      }
+    }
+    
+    super.applicationDidEnterBackground(application)
   }
 }
