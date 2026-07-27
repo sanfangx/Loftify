6a766c5486a6d460614295a0236d95303656f579
sanfangx <x340711@icloud.com>
2026-07-27 13:17:36 +0800

    Revert "fix: aggressively clear image cache when app enters background (paused/hidden) to guarantee iOS doesn't kill it for dirty memory"
    This reverts commit a643f48f237490e1316f4a22d9ca25430a542b53.

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -641,15 +641,11 @@ class MainScreenState extends State<MainScreen>
         cancleTimer();
         break;
       case AppLifecycleState.paused:
-        PaintingBinding.instance.imageCache.clear();
-        PaintingBinding.instance.imageCache.clearLiveImages();
         setTimer();
         break;
       case AppLifecycleState.detached:
         break;
       case AppLifecycleState.hidden:
-        PaintingBinding.instance.imageCache.clear();
-        PaintingBinding.instance.imageCache.clearLiveImages();
         break;
     }
   }
